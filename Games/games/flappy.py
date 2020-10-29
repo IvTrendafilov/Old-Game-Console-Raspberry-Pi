d302f8919380013ed819b6a94511fcec293cdf11
Pavel Hristov <p.hristov@student.utwente.nl>
2020-10-29 18:05:42 +0100

    FLAPPY BIRD FIXED

--- Games/games/flappy.py
+++ Games/games/flappy.py
@@ -2,13 +2,13 @@ import shelve
 import time
 import pygame, sys, random
 from .settings import *
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 def draw_pillars(pillars):
     global pillar_surface
     for pillar in pillars:
-        if pillar.bottom >= HEIGHT:
+        if pillar.bottom >= HEIGHT - 124:
             screen.blit(pillar_surface, pillar)
         else:
             flip_pillar = pygame.transform.flip(pillar_surface, False, True)
@@ -18,16 +18,19 @@ def draw_pillars(pillars):
 def draw_floor():
     global floor_surface
     global floor_x_pos
-    screen.blit(floor_surface, (floor_x_pos, HEIGHT - 124))
-    screen.blit(floor_surface, (floor_x_pos + WIDTH, HEIGHT - 124))
+    screen.blit(floor_surface, (floor_x_pos, HEIGHT/1.2))
+    screen.blit(floor_surface, (floor_x_pos + WIDTH, HEIGHT/1.2))
 
 
 def create_pillar():
     global pillar_surface
     global pillar_height
-    random_pillar_height = random.choice(pillar_height)
+    random_dividedPillar = random.randint(13, 30)
+    random_dividedPillar = float(random_dividedPillar / 10)
+    random_pillar_height = HEIGHT / random_dividedPillar
+    #random_pillar_height = random.choice(pillar_height)
     bottom_pillar = pillar_surface.get_rect(midtop=(WIDTH + 124, random_pillar_height))
-    top_pillar = pillar_surface.get_rect(midbottom=(WIDTH + 124, random_pillar_height - 300))
+    top_pillar = pillar_surface.get_rect(midbottom=(WIDTH + 124, random_pillar_height - HEIGHT/3.5))
     return bottom_pillar, top_pillar
 
 
@@ -42,8 +45,10 @@ def check_collision(pillars):
     collision = False
     for pillar in pillars:
         if bird_rectangle.colliderect(pillar):
+            hit_sound.play()
             collision = True
-    if bird_rectangle.top <= -100 or bird_rectangle.bottom >= 900:
+    if bird_rectangle.top <= -HEIGHT/10 or bird_rectangle.bottom >= HEIGHT/1.23:
+        hit_sound.play()
         collision = True
     return collision
 
@@ -59,7 +64,7 @@ def bird_animation():
     global bird_index
     global bird_rectangle
     new_bird = bird_frames[bird_index]
-    new_bird_rect = new_bird.get_rect(center=(100, bird_rectangle.centery))
+    new_bird_rect = new_bird.get_rect(center=(WIDTH/5, bird_rectangle.centery))
     return new_bird, new_bird_rect
 
 
@@ -79,7 +84,7 @@ def show_score(game_status):
         score_type = f'{int(score)}'
     global game_font
     score_surface = game_font.render(score_type, True, (255, 255, 255))
-    score_rectangular = score_surface.get_rect(center=(288, 100))
+    score_rectangular = score_surface.get_rect(center=(WIDTH, HEIGHT/1.7))
     screen.blit(score_surface, score_rectangular)
 
 
@@ -87,6 +92,7 @@ def show_score(game_status):
 
 def main():
     # Game initialization
+    pygame.mixer.pre_init(44100, 16, 2, 512)
     pygame.init()
 
     clock = pygame.time.Clock()
@@ -110,16 +116,23 @@ def main():
     bird_pos_x = 100
     bird_pos_y = 512
     pillar_spawn_interval = 1200
+    ##############################SOUND##############################
+    global bird_sound
+    bird_sound = pygame.mixer.Sound("./games/assets/sfx_wing.wav")
+    global point_sound
+    point_sound = pygame.mixer.Sound("./games/assets/sfx_point.wav")
+    global hit_sound
+    hit_sound = pygame.mixer.Sound("./games/assets/sfx_hit.wav")
 
     ##############################SURFACES############################
 
     # Background surface
     background_surface = pygame.image.load('./games/assets/background-day.png').convert()
-    background_surface = pygame.transform.scale2x(background_surface)
+    background_surface = pygame.transform.rotozoom(background_surface, 0, 2.5)
 
     # Game Over surface
     gameOver_surface = pygame.image.load('./games/assets/gameover.png').convert_alpha()
-    gameOver_surface = pygame.transform.scale2x(gameOver_surface)
+    #gameOver_surface = pygame.transform.scale2x(gameOver_surface)
     game_over_rectangle = gameOver_surface.get_rect(center=(WIDTH / 2, HEIGHT / 2))
 
     # Floor surface
@@ -145,7 +158,11 @@ def main():
     bird_index = 0
     bird_surface = bird_frames[bird_index]
     global bird_rectangle
-    bird_rectangle = bird_surface.get_rect(center=(100, 512))
+    global bird_height
+    bird_height = HEIGHT / 2.5
+    global bird_width
+    bird_width = WIDTH / 5.7
+    bird_rectangle = bird_surface.get_rect(center=(bird_width,bird_height))
 
     BIRDFLAP = pygame.USEREVENT + 1
 
@@ -155,31 +172,31 @@ def main():
     # Pillar Surface
     global pillar_surface
     pillar_surface = pygame.image.load('./games/assets/pipe-green.png').convert()
-    pillar_surface = pygame.transform.scale2x(pillar_surface)
+    #pillar_surface = pygame.transform.scale2x(pillar_surface)
     pillar_list = []
     SPAWNPILLAR = pygame.USEREVENT
     pygame.time.set_timer(SPAWNPILLAR, pillar_spawn_interval)
     global pillar_height
-    pillar_height = [400, 600, 800]
+    #pillar_height = [400, 600, 800]
     passed_pillars = []
 
     while True:
 
-        # GPIO.setup(15, GPIO.OUT)
-        # GPIO.output(15, GPIO.LOW)
-        # GPIO.setup(15, GPIO.IN)  # ARROW UP
-        # if GPIO.input(15) and game_active:
-        #     bird_movement = 0
-        #     bird_movement -= 10
-        #     time.sleep(0.1)
-        # if GPIO.input(15) and game_active==False:
-        #     game_active = True
-        #     pillar_list.clear()
-        #     score = 0
-        #     passed_pillars.clear()
-        #     bird_rectangle.center = (100, 512)
-        #     bird_movement = 0
-        #     time.sleep(0.1)
+        GPIO.setup(15, GPIO.OUT)
+        GPIO.output(15, GPIO.LOW)
+        GPIO.setup(15, GPIO.IN)  # ARROW UP
+        if GPIO.input(15) and game_active:
+            bird_movement = 0
+            bird_movement -= 10
+            time.sleep(0.1)
+        if GPIO.input(15) and game_active==False:
+            game_active = True
+            pillar_list.clear()
+            score = 0
+            passed_pillars.clear()
+            bird_rectangle.center = (100, 512)
+            bird_movement = 0
+            time.sleep(0.1)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -188,14 +205,14 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE and game_active:
                     bird_movement = 0
-                    bird_movement -= 10
+                    bird_movement -= 8
 
                 if event.key == pygame.K_SPACE and game_active == False:
                     game_active = True
                     pillar_list.clear()
                     score = 0
                     passed_pillars.clear()
-                    bird_rectangle.center = (100, 512)
+                    bird_rectangle.center = (bird_width,bird_height)
                     bird_movement = 0
 
             if event.type == SPAWNPILLAR:
@@ -229,6 +246,7 @@ def main():
                 if bird_pos_x > pillar.centerx:
                     if pillar not in passed_pillars:
                         passed_pillars.append(pillar)
+                        point_sound.play()
         else:
             return score
             # screen.blit(game_idle_image, game_idle_rectangle)
